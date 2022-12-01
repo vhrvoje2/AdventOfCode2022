b04b805d9f93eb92fb99dba98888bdf0c4a8be55
vhrvoje2 <vhrvoje2@yahoo.com>
2022-12-01 20:04:15 +0100

    Day 1 solutions

---
+++ template.py
@@ -0,0 +1,20 @@
+from timeit import Timer
+
+placeholder = []
+with open(".txt", "r") as file:
+    for item in file.readlines():
+        placeholder.append(item.strip())
+
+
+def part1():
+    pass
+    # print(f"Part 1. solution {}")
+
+
+def part2():
+    pass
+    # print(f"Part 2. solution {}")
+
+
+print(f"Execution time: {Timer(part1).timeit(number=1)}")
+print(f"Execution time: {Timer(part2).timeit(number=1)}")

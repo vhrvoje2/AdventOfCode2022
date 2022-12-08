ecf6118d96151e3cbbbb1f2578b817eb8f35c9b4
vhrvoje2 <vhrvoje2@yahoo.com>
2022-12-08 23:51:01 +0100

    Day 8 Part 2 solution

--- Day8/day8.py
+++ Day8/day8.py
@@ -51,9 +51,47 @@ def part1():
 
 
 def part2():
-    solution = None
-    print(f"Part 2. solution {solution}")
+    max_scenic_score = 0
+
+    for y in range(1, len(forest) - 1):
+        for x in range(1, len(forest) - 1):
+            visible_N = visible_S = visible_E = visible_W = 0
+            current_tree = forest[y][x]
+
+            for n in range(y - 1, -1, -1):
+                if forest[n][x] >= current_tree:
+                    visible_N += 1
+                    break
+                else:
+                    visible_N += 1
+
+            for s in range(y + 1, len(forest)):
+                if forest[s][x] >= current_tree:
+                    visible_S += 1
+                    break
+                else:
+                    visible_S += 1
+
+            for w in range(x - 1, -1, -1):
+                if forest[y][w] >= current_tree:
+                    visible_W += 1
+                    break
+                else:
+                    visible_W += 1
+
+            for e in range(x + 1, len(forest)):
+                if forest[y][e] >= current_tree:
+                    visible_E += 1
+                    break
+                else:
+                    visible_E += 1
+
+            scenic_score = visible_N * visible_S * visible_E * visible_W
+            if scenic_score > max_scenic_score:
+                max_scenic_score = scenic_score
+
+    print(f"Part 2. solution {max_scenic_score}")
 
 
 print(f"Execution time: {Timer(part1).timeit(number=1)}")
-# print(f"Execution time: {Timer(part2).timeit(number=1)}")
+print(f"Execution time: {Timer(part2).timeit(number=1)}")

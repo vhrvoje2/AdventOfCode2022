81c0eb92230ff23a009c7e724464492752e0352a
vhrvoje2 <vhrvoje2@yahoo.com>
2022-12-10 21:53:56 +0100

    Day 10 Part 2 solution

--- Day10/day10.py
+++ Day10/day10.py
@@ -1,5 +1,4 @@
 from timeit import Timer
-from math import sqrt, pow
 
 program = []
 with open("input10.txt", "r") as file:
@@ -50,7 +49,13 @@ def print_screen(screen):
         print(screen[_])
 
 
+def draw_crt(CRT):
+    print_screen(CRT)
+
+
 def part2():
+    print(f"Part 2. solution")
+
     CRT = [
         ["." for _ in range(40)],
         ["." for _ in range(40)],
@@ -60,38 +65,46 @@ def part2():
         ["." for _ in range(40)],
     ]
 
-    n = 0
-    signal_strength = 0
+    crt_pos = {"x": 0, "y": 0}
     cycle_count = 0
     register_X = 1
+    sprite_pos = {"x": register_X, "y": 0}
 
     for line in program:
         try:
             instruction, value = line.split(" ")
             cycle_count += 1
-            if cycle_count == 20:
-                signal_strength += 20 * register_X
-            elif cycle_count % (60 + n * 40) == 0:
-                signal_strength += cycle_count * register_X
-                n += 1
+            crt_pos["x"] = cycle_count % 40 - 1
+            sprite_pos["x"] = register_X % 40
+            sprite_x3 = [sprite_pos["x"] - 1, sprite_pos["x"], sprite_pos["x"] + 1]
+            if crt_pos["x"] in sprite_x3:
+                CRT[crt_pos["y"]][crt_pos["x"]] = "O"
+            if cycle_count % 40 == 0:
+                crt_pos["y"] += 1
+                sprite_pos["y"] += 1
             cycle_count += 1
-            if cycle_count == 20:
-                signal_strength += 20 * register_X
-            elif cycle_count % (60 + n * 40) == 0:
-                signal_strength += cycle_count * register_X
-                n += 1
+            crt_pos["x"] = cycle_count % 40 - 1
+            sprite_pos["x"] = register_X % 40
+            sprite_x3 = [sprite_pos["x"] - 1, sprite_pos["x"], sprite_pos["x"] + 1]
+            if crt_pos["x"] in sprite_x3:
+                CRT[crt_pos["y"]][crt_pos["x"]] = "O"
+            if cycle_count % 40 == 0:
+                crt_pos["y"] += 1
+                sprite_pos["y"] += 1
             register_X += int(value)
         except ValueError:
             # noop
             cycle_count += 1
-            if cycle_count == 20:
-                signal_strength += 20 * register_X
-            elif cycle_count % (60 + n * 40) == 0:
-                signal_strength += cycle_count * register_X
-                n += 1
+            crt_pos["x"] = cycle_count % 40 - 1
+            sprite_pos["x"] = register_X % 40
+            sprite_x3 = [sprite_pos["x"] - 1, sprite_pos["x"], sprite_pos["x"] + 1]
+            if crt_pos["x"] in sprite_x3:
+                CRT[crt_pos["y"]][crt_pos["x"]] = "O"
+            if cycle_count % 40 == 0:
+                crt_pos["y"] += 1
+                sprite_pos["y"] += 1
 
-    print(f"Part 2. solution")
-    print_screen(CRT)
+    draw_crt(CRT)
 
 
 print(f"Execution time: {Timer(part1).timeit(number=1)}")
